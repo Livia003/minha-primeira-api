91a222eb2db47a6cf9287b00f0ed0c5c9494051a
Lívia Araújo <liviaraj93@gmail.com>
2024-02-13 03:23:35 -0300

    Apagando a pasta "mulher"

--- mulheres.js
+++ mulheres.js
@@ -5,13 +5,6 @@ const app = express();
 const porta = 3333;
 
 const mulheres = [
-    {
-        nome: 'Lívia Rocha',
-
-        imagem: 'https://media.licdn.com/dms/image/D4D03AQFoGQiudYM4Xw/profile-displayphoto-shrink_400_400/0/1701909327555?e=1713398400&v=beta&t=oS2hhAOetWhJAgEf90A6U1gad38X31kUyHlwdU_e0eE',
-
-        minibio: 'Líder Regional.'
-    },
     
     {
 
